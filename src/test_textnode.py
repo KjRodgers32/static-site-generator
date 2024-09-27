f48ee9fc1b8e5ecd5a6bd57f17fdddedb72fd4b2
KjRodgers32 <kjrodgers26@gmail.com>
2024-09-27 18:52:22 -0400

    added file to test TextNode class

---
+++ src/test_textnode.py
@@ -0,0 +1,37 @@
+import unittest
+
+from TextNode import TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node1 = TextNode("This is a text node", "bold", "google.com")
+        node2 = TextNode("This is a text node", "bold", "google.com")
+        self.assertEqual(node1, node2)
+    
+    def test_eq_text_not_equal(self):
+        node1 = TextNode("This is a text node", "bold", "google.com")
+        node2 = TextNode("This is a new text node", "bold", "google.com")
+        self.assertNotEqual(node1, node2)
+    
+    def test_eq_text_type_not_equal(self):
+        node1 = TextNode("This is a text node", "bold", "google.com")
+        node2 = TextNode("This is a text node", "italics", "google.com")
+        self.assertNotEqual(node1, node2)
+    
+    def test_eq_url_not_equal(self):
+        node1 = TextNode("This is a text node", "bold", "google.com")
+        node2 = TextNode("This is a text node", "bold", "bing.com")
+        self.assertNotEqual(node1, node2)
+    
+    def test_eq_missing_requirement(self):
+        with self.assertRaises(Exception) as type_error:
+            node1 = TextNode("bold", "google.com")
+        print(type_error.exception.args)
+        self.assertRegex(type_error.exception.args[0], "missing 1 required positional argument")
+
+    def test_repr(self):
+        node = TextNode("This is a text node", "bold", "google.com")
+        self.assertEqual(print('TestNode(This is a text node, bold, google.com)'), print(node))
+
+if __name__ == "__main__":
+    unittest.main()
