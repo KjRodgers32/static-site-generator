ff7f1d28848f2f67927991afddb3322e0ed7f214
KjRodgers32 <kjrodgers26@gmail.com>
2024-09-28 13:03:51 -0400

    added method to turn props dictionary to a string of html atributes

--- src/HtmlNode.py
+++ src/HtmlNode.py
@@ -7,3 +7,13 @@ class HtmlNode:
 
     def to_html(self):
         raise NotImplementedError
+    
+    def props_to_html(self):
+        string = ''
+        length_of_props = len(self.props)
+        for k, v in self.props.items():
+            string += f'{k}="{v}"'
+            if length_of_props > 1:
+                string += ' '
+                length_of_props -= 1
+        return string
