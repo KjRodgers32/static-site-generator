78ae9d94642330145139705a9ad73265dbd72255
Kevin <kjrodgers26@gmail.com>
2024-09-29 21:58:01 -0400

    finished method to turn leafnode into html

--- src/LeafNode.py
+++ src/LeafNode.py
@@ -1,9 +1,14 @@
 from HtmlNode import HtmlNode
 
 class LeafNode(HtmlNode):
-    def __init__(self, tag, value, props):
+    def __init__(self, tag=None, value=None, props=None,):
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
         if self.value == None:
             raise ValueError("Leaf Node must have a value!")
+        if self.tag == None:
+            return f"{self.value}"
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
