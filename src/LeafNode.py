26a9ca211143fe3c1479e6db3287d6bb639b1955
Kevin <kjrodgers26@gmail.com>
2024-09-29 23:35:44 -0400

    simplified to_html method

--- src/LeafNode.py
+++ src/LeafNode.py
@@ -1,14 +1,12 @@
 from HtmlNode import HtmlNode
 
 class LeafNode(HtmlNode):
-    def __init__(self, tag=None, value=None, props=None,):
-        super().__init__(tag=tag, value=value, props=props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.value == None:
+        if self.value == None or self.value.strip() == '':
             raise ValueError("Leaf Node must have a value!")
         if self.tag == None:
             return f"{self.value}"
-        if self.props == None:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
