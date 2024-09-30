47930f48cf1ad9c7e166d41921c094b897a43a91
Kevin <kjrodgers26@gmail.com>
2024-09-29 21:58:43 -0400

    added test to check edge cases of to_html method

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -8,8 +8,19 @@ class TestLeafNode(unittest.TestCase):
     
     def test_to_html_no_value(self):
         with self.assertRaises(ValueError):
-            leaf_node = LeafNode(tag="a", props={"href": "google.com"}) 
-            self.assertRaises(ValueError("Leaf Node must have a value!"), leaf_node.to_html())
+            self.assertRaises(ValueError("Leaf Node must have a value!"),LeafNode(tag="a", props={"href": "google.com"}).to_html())
+
+    def test_to_html_no_tag(self):
+        leaf_node = LeafNode(value="Click")
+        self.assertEqual("Click", leaf_node.to_html())
+
+    def test_to_html_no_props(self):
+        leaf_node = LeafNode(tag="p", value="This is a paragraph")
+        self.assertEqual("<p>This is a paragraph</p>", leaf_node.to_html())
+
+    def test_to_html(self):
+        leaf_node = LeafNode(tag="a", value="Click Me", props={"href": "google.com"})
+        self.assertEqual('<a href="google.com">Click Me</a>', leaf_node.to_html())
 
 
 if __name__ == "__main__":
