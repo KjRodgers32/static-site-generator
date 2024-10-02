a1ca1642917f461a9778d7b05dbd1cb8a0e371c1
KjRodgers32 <kjrodgers26@gmail.com>
2024-10-02 16:44:50 -0400

    added split_nodes_delimeter function to create LeafNodes from text nodes

--- src/main.py
+++ src/main.py
@@ -17,9 +17,19 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", '', {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("TextNode is invalid")
+    
+def split_nodes_delimiter(old_nodes ,delimiter, text_type):
+    nodes_array = []
+    for node in old_nodes:
+        string_array = node.text.split(delimiter)
+        nodes_array.append(TextNode(string_array[0], "text"))
+        nodes_array.append(TextNode(string_array[1], text_type))
+        nodes_array.append(TextNode(string_array[2], "text"))
+    return nodes_array
 
 def main():
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev.com")
-    print(text_node)
+    node = TextNode("This is text with a `code block` word", "text")
+    new_nodes = split_nodes_delimiter([node], "`", "code")
+    print(new_nodes)
 
 main()
