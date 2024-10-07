d6501b07b834721453bbefa4c93c199b2835e82e
Kevin <kjrodgers26@gmail.com>
2024-10-06 23:03:47 -0400

    added functions for block markdown elements

---
+++ src/block_markdown_functions.py
@@ -0,0 +1,19 @@
+import re
+
+def markdown_to_blocks(markdown):
+    markdown_blocks = markdown.split('\n')
+    return [x for x in markdown_blocks if x.strip() != ""]
+
+def block_to_block_type(markdown):
+    if markdown.startswith("#"):
+        return "heading"
+    if markdown.startswith("```"):
+        return "code"
+    if markdown.startswith(">"):
+        return "quote"
+    if markdown.startswith("*") or markdown.startswith("-"):
+        return "unordered list"
+    if re.match(r"^[0-9]+.", markdown):
+        return "ordered list"
+    else:
+        return "paragraph"
